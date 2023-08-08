76cbbbc7ca338902988251f3aeca3496dd0c7816
Loïc Lord <lordloyc@gmail.com>
2023-08-08 13:15:59 -0400

    added test for printing bureaucrat

---
+++ mod5/ex01/Settings.mk
@@ -0,0 +1,23 @@
+#------------------------------------------------------------------------------#
+#                              PROJECT VARIABLES                               #
+#------------------------------------------------------------------------------#
+
+# Executable name
+NAME		=	AnimalFarm
+
+# Source file names (prefix their subdir if needed)
+FILES		=	main \
+				Bureaucrat \
+
+# Directory names
+SRCDIR		=
+OBJDIR		=
+#TSTDIR		=	tests \
+#SUBDIRS	=	example \
+
+# Libraries (.a files) to include for compilation
+LIBS		=
+LIBX		=
+
+# Arguments to call the command with
+ARGS		=
