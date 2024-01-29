9f01bc2222fa94ced954a0fb2fc6a88311e821f8
Loïc Lord <lordloyc@gmail.com>
2024-01-29 17:19:29 -0500

    minor

--- mod6/ex00/main.cpp
+++ mod6/ex00/main.cpp
@@ -2,19 +2,6 @@
 #include "ScalarConverter.hpp"
 
 
-void	test( void )
-{
-	std::cout << "\nO================================ TEST 1 ================================O\n"  << std::endl;
-	{
-
-	}
-	std::cout << "\nO================================ TEST 2 ================================O\n"  << std::endl;
-	{
-
-	}
-	std::cout << "\nO================================= END ==================================O\n\n" << std::endl;
-}
-
 int		main( int ac, char **av )
 {
 	if ( ac != 2 ) { std::cout << "Usage: ./scalar [string]" << std::endl;  return 1; }
